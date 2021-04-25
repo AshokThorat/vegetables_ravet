4aeabefff23573aed365af8f7a3c60bfc7196ae5
Ashok <ashokmthorat@gmail.com>
2021-04-25 21:58:12 +0530

    mac

--- lib/api_services/CityApi.dart
+++ lib/api_services/CityApi.dart
@@ -11,7 +11,7 @@ class CityApi
       List<CityModels> city=cityModelsFromJson(response.body);
       return city;
     } catch (e) {
-      print('ubuntu response $e');
+      print('mac response $e');
     }
   }
 }
